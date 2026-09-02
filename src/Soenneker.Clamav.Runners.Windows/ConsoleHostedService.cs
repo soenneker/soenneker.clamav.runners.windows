f25bd0d03023574d2de34eef3a6339e235bc6c89
Jake Soenneker <jake@soenneker.com>
2026-09-02 15:10:00 -0500

    Publish Windows FreshClam package

--- src/Soenneker.Clamav.Runners.Windows/ConsoleHostedService.cs
+++ src/Soenneker.Clamav.Runners.Windows/ConsoleHostedService.cs
@@ -37,6 +37,8 @@ public sealed class ConsoleHostedService : IHostedService
                 string stageDirectory = await _fileOperationsUtil.Process(cancellationToken).NoSync();
                 await _runnersManager.PushIfChangesNeededForDirectory(Path.Combine(Constants.RuntimeIdentifier, "clamav"), stageDirectory,
                     Constants.Library, $"https://github.com/soenneker/{Constants.Library}", false, cancellationToken).NoSync();
+                await _runnersManager.PushIfChangesNeededForDirectory(Path.Combine(Constants.RuntimeIdentifier, "freshclam"), stageDirectory,
+                    Constants.FreshclamLibrary, $"https://github.com/soenneker/{Constants.FreshclamLibrary}", false, cancellationToken).NoSync();
                 _exitCode = 0;
             }
             catch (Exception exception)
@@ -44,7 +46,7 @@ public sealed class ConsoleHostedService : IHostedService
                 if (Debugger.IsAttached)
                     Debugger.Break();
 
-                _logger.LogError(exception, "Could not update {Library}", Constants.Library);
+                _logger.LogError(exception, "Could not update the Windows ClamAV packages");
                 _exitCode = 1;
             }
             finally
--- src/Soenneker.Clamav.Runners.Windows/Constants.cs
+++ src/Soenneker.Clamav.Runners.Windows/Constants.cs
@@ -3,5 +3,6 @@ namespace Soenneker.Clamav.Runners.Windows;
 public static class Constants
 {
     public const string Library = "Soenneker.Clamav.Windows";
+    public const string FreshclamLibrary = "Soenneker.Clamav.Freshclam.Windows";
     public const string RuntimeIdentifier = "win-x64";
 }
